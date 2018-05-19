02dadfd71bdfa19b3c48da4d6969dd97139ccf47
Tajammul <tpzuffari@gmail.com>
2018-05-19 23:14:28 +0530

    Window objects

--- 3-Revisiting-very-basics-3/app1.js
+++ 3-Revisiting-very-basics-3/app1.js
@@ -11,5 +11,28 @@ val = today;
 let birthday = new Date('01/07/1996')
 
 
-console.log(birthday)
-console.log(val)
+// console.log(birthday)
+// console.log(val)
+
+// FUNCTION DECLARATION
+
+function greet(firstName = 'Awesome', lastName= 'Person') {
+    return `Hello ${firstName} ${lastName}`
+}
+
+// console.log(greet('Tajammul', 'Pasha'));
+// console.log(greet())
+
+// FUNCTION EXPRESSIONS 
+
+const square = function(x = 1){
+    return x*x
+};
+
+// console.log(square(12))
+
+// IMMEDIATELY INVOKED FUNCTION EXPRESSION - IIFEs
+
+// (function(expr){
+//     console.log(`IIFE ${expr}`)
+// })('run');
---
+++ 3-Revisiting-very-basics-3/window.js
@@ -0,0 +1,39 @@
+ // WINDOW METHODS / OBJECTS / PROPERTIES
+
+ // OUTER HEIGHT AND WIDTH
+
+ let val;
+
+ val = window.outerHeight
+ val = window.outerWidth
+
+ // INNER HEIGHT AND WIDTH
+
+ val = window.innerHeight
+ val = window.innerWidth
+
+ // SCROLL POINTS
+
+ val = window.scrollY
+ val = window.scrollX
+
+ // LOCATION OBJECT
+
+ val = window.location;
+ val = window.location.hostname;
+ val = window.location.port;
+ val = window.location.href;
+
+ // REDIRECT
+//  window.location.href = 'http://google.com'
+// RELOAD
+// window.location.reload();
+
+
+// History Object
+// window.history.go(-2)
+
+// Navigator Object
+val = window.navigator
+
+ console.log(val);
